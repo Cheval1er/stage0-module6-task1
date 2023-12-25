a753809bb978e7c2f075be4775db18a81555bbea
nutsa.matchavariani <nutsa.matchavariani.1@gmail.com>
2023-12-25 22:45:46 +0400

    Module #6. Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,10 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird() {
+        // Call the Animal class constructor with specific values for Bird
+        super("blue", 2, false);
+    }
+
 
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    
+    
+    
 
+    public Dog() {
+        // Call the Animal class constructor with specific values for Dog
+        super("brown", 4, true);
+    }
 }
